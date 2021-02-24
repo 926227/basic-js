921de3dc86cb3815ae79f390aff8114290ccad09
konstantin zhilyaev <k926227@gmail.com>
2021-02-24 15:13:39 +0300

    feat: task **transform-array** done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError('Not Array');
+  }
+  let arrResult = arr.slice();
+  for (let i = 0; i< arrResult.length; i++) {
+    switch (arrResult[i]) {
+      case '--discard-next': 
+        arrResult[i] = null;
+        arrResult[i+1]= null;
+        break;
+      case '--discard-prev':
+        arrResult[i] = null;
+        arrResult[i-1]= null;
+        break;
+      case '--double-next':
+        if (i != arrResult.length-1) {
+          arrResult[i] = arrResult[i+1];
+        } else {
+          arrResult[i] = null;
+        }
+        break;
+      case '--double-prev':
+        if (i) {
+          arrResult[i] = arrResult[i-1];
+        } else {
+          arrResult[i] = null;
+        }
+        break;
+    }
+  }
+  return arrResult.filter(item => item != null);
 };
