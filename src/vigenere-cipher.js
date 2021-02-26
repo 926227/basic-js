4df3df60333c871b5f5e7c2560c4e82e6e42a0d0
konstantin zhilyaev <k926227@gmail.com>
2021-02-26 10:35:53 +0300

    feat: task **vigenere-cipher** done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(direct = true) {
+    this._DIRECT = direct;
+    this._ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  
+  encrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new TypeError();
+    }
+    let result = '';
+    str = str.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+    
+    for (let i = 0; i < str.length; i++) {
+      if (this._ALPHABET.includes(str[i])) {
+        if (key.length < str.length) {
+          key.push(key[i]);
+        }
+        let strI = this._ALPHABET.indexOf(str[i]);
+        let keyI = this._ALPHABET.indexOf(key[i]);
+        result += this._ALPHABET[(((this._ALPHABET.length + (strI + keyI)) % this._ALPHABET.length))];
+      } else {
+        result +=str[i];
+        key.splice(i,0, ' ');
+      }
+    }
+    
+    return this._DIRECT ? result : result.split('').reverse().join('');
+  }
+  decrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new TypeError();
+    }
+    let result = '';
+    str = str.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+    for (let i = 0; i < str.length; i++) {
+      if (this._ALPHABET.includes(str[i])) {
+        if (key.length < str.length) {
+          key.push(key[i]);
+        }
+        let strI = this._ALPHABET.indexOf(str[i]);
+        let keyI = this._ALPHABET.indexOf(key[i]);
+        result += this._ALPHABET[(((this._ALPHABET.length + (strI - keyI)) % this._ALPHABET.length))];
+      } else {
+        result +=str[i];
+        key.splice(i,0, ' ');
+      }
+    }
+    return this._DIRECT ? result : result.split('').reverse().join('');
   }
 }
 
